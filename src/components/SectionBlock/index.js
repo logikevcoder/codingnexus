0216563646d347155ac601e183ebc9b70981001c
Kevin Napier <kevin.napier@rotary.org>
2021-02-01 21:44:56 -0600

    Built the basic section block component, added new section blog to home page

---
+++ src/components/SectionBlock/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../styles/tailwind.css';
+
+const SectionBlock = ({ children }) => {
+  return <section>{children}</section>;
+};
+
+export default SectionBlock;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/tailwind.css';
+import SectionBlock from '../components/SectionBlock/index.js';
 
 import Layout from '../layouts/layout';
 import Image from '../components/image';
@@ -13,6 +14,7 @@ const IndexPage = () => (
     <div>
       <Image />
     </div>
+    <SectionBlock />
   </Layout>
 );
 
